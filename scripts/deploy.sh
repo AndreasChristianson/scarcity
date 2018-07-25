36e108f1a351f652100c1de94cee263de6786f69
Andreas <achristianson@sourceallies.com>
2018-07-25 00:34:32 -0500

    setting up migrations via flyway

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,7 @@
 #! /bin/bash
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-export REPO=andreaschristianson/scarcity
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker build -t $REPO:$COMMIT .
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
-docker push $REPO
-
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+
   if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
     rm -rf $HOME/google-cloud-sdk;
     export CLOUDSDK_CORE_DISABLE_PROMPTS=1;
@@ -30,5 +23,5 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 
   gcloud container clusters get-credentials $GCLOUD_CLUSTER_NAME
 
-  envsubst < k8s/deploy.yml | kubectl apply -f - 
+  envsubst < k8s/deploy.yml | kubectl apply -f -
 fi
--- src/server/pg/pg-route.js
+++ src/server/pg/pg-route.js
@@ -2,7 +2,7 @@ export default ({
     method: 'GET',
     path: '/pg-test',
     handler: async (request, h) => {
-        const {rows} = await h.pg.query('SELECT NOW()');
+        const {rows} = await h.pg.query('SELECT * from flyway_schema_history');
 
         return h.response(rows).code(200);
     }
